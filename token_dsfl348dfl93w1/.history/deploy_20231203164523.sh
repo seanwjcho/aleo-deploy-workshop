975d929a15480e6181a10b09fb55663c318800f8
seanw <seanwjcho@gmail.com>
2023-12-03 17:10:06 -0500

    added code

---
+++ token_dsfl348dfl93w1/.history/deploy_20231203164523.sh
@@ -0,0 +1,5 @@
+PRIVATEKEY=""
+
+APPNAME="token_dsfl348dfl93w1"
+
+cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 1000000
